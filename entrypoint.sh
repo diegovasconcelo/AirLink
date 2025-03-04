50c31fafe933d7eeb271e7a6425174fa0dc4d8d7
diegovasconcelo <diegovasconcelo@icloud.com>
2025-03-03 23:34:08 -0300

    Add fixture loading option to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,9 @@ set -o nounset
 python manage.py migrate
 python manage.py collectstatic --noinput
 
+if [ "${LOAD_FIXTURES:-}" = "True" ]; then
+    echo "Loading fixtures..."
+    python manage.py loaddata data/fixtures/journeys/data.json
+fi
+
 python -m gunicorn core.asgi:application -k uvicorn.workers.UvicornWorker -b=0.0.0.0:8000
