841f3a495f809654825538cada8679c74709c646
diegovasconcelo <diegovasconcelo@icloud.com>
2025-03-03 22:38:27 -0300

    Add Docker support with Dockerfile, .Dockerignore, and entrypoint script

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# if any of the commands fails, the entire script fails.
+set -o errexit
+# exits if any variable is not set.
+set -o nounset
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+python -m gunicorn core.wsgi:application -b=0.0.0.0:8000
